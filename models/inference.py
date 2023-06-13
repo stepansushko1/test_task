338d2e94b744d5f438e887447491ca9438738949
stepansushko1 <stepan_sushko@ukr.net>
2023-06-13 21:00:00 +0300

    changing eda

--- models/inference.py
+++ models/inference.py
@@ -14,68 +14,57 @@ preprocessing_dir = os.path.join(current_dir, "../data_processing")
 
 sys.path.append(preprocessing_dir)
 
-os.chdir("../..") # need to be in root directory test_task if you then don't need this
-
 from constansts import *
 
+# os.chdir("../..") # need to be in root directory test_task if you then don't need this
+
 def read_img(filename: str) -> np.ndarray:
-    """Reads image from file, and clips its' values to be in [0, 1] range"""
     image = cv2.imread(filename)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     return np.float32(image / 255)
 
-def select_random_files(directory, n:int):
+def select_random_files(n:int):
     file_list = []
     curr_dir = os.path.join(current_dir, IMAGES_PATH_TEST)
-    print('sjnhdfjshdfojsjdofs', curr_dir)
     entries = os.listdir(curr_dir)
     random_entries = random.sample(entries, n)
 
     for entry in random_entries:
         file_list.append(read_img(os.path.join(curr_dir, entry)))
 
-
     return np.stack(file_list)
 
-def check_results():
+def plot_helper(ax:any, indx:int, title_name:str):
+    ax[indx].set_title(title_name, fontsize=14)
+    ax[indx].axis('off')
+    ax[indx].grid(False)
+
+def check_results(number_of_examples:int):
 
     model = U_NET_fullresolution(1,True)
     model.load_weights("models/weights_unet.h5")
 
-
-    test_imgs = select_random_files(IMAGES_PATH_TEST, 10)
+    test_imgs = select_random_files(number_of_examples)
     masks = model(test_imgs, training=False)
     
 
     for i, img in enumerate(test_imgs):
-        fig, ax = plt.subplots(1, 3, figsize=(12, 8))
+        fig, ax = plt.subplots(1, 2, figsize=(12, 8))
         fig.suptitle('Prediction results', fontsize=18)
 
         # plot orig image
         ax[0].imshow(img)
-        ax[0].set_title('Original image', fontsize=14)
-        ax[0].axis('off')
-        ax[0].grid(False)
+        plot_helper(ax, 0, "Image")
         
         # plot predicted mask
         mask = masks[i].numpy()
-        print(mask)
         ax[1].imshow(mask, 'binary_r')
-        ax[1].set_title('Predicted mask', fontsize=14)
-        ax[1].axis('off')
-        ax[1].grid(False)
-
-        # plot both image and mask
-        ax[2].imshow(img)
-        ax[2].imshow(mask, 'binary_r', alpha=0.4)
-        ax[2].set_title('Image and mask', fontsize=14)
-        ax[2].axis('off')
-        ax[2].grid(False)
+        plot_helper(ax, 1, 'UNET mask')
         fig.tight_layout()
 
         plt.savefig(os.path.join(current_dir, f"results/res{i}.jpg"), bbox_inches='tight')
 
-
-check_results()
+if __name__ == "__main__":
+    check_results(10)
 
 
